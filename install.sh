5003f5bd7a1b42daedab19c04087c20a2d659a21
Florian Ludwig <f.ludwig@greyrook.com>
2022-04-05 10:54:22 +0200

    install via dnf

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
+# install:
+# autojump
+#  https://github.com/wting/autojump
+# wl-clipboard
+#  https://github.com/bugaevc/wl-clipboard
+# lsd
+#  https://github.com/Peltoche/lsd
+sudo dnf install -y autojump autojump-zsh wl-clipboard lsd
+
+
+
 ln -s `pwd`/.bashrc ~/.bashrc
+ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.gitconfig ~/.gitconfig
